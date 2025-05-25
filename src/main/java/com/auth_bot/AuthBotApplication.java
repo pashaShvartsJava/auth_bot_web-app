8fe7b530e7e58d7049a1eb5e21cdfa3fb8b1568d
Pavel Shvarts <ctrade.shvarts@gmail.com>
2025-05-25 13:47:50 +0200

    connected datbase

---
+++ src/main/java/com/auth_bot/AuthBotApplication.java
@@ -0,0 +1,13 @@
+package com.auth_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth_bot/AuthBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
